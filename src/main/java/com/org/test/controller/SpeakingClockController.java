f10dee818ad75d3f02168b6da45981089df8121c
GaneshPawar333 <ganesh.pawar@echofeel.com>
2023-07-27 17:18:59 +0530

    initialcode

---
+++ src/main/java/com/org/test/controller/SpeakingClockController.java
@@ -0,0 +1,27 @@
+package com.org.test.controller;
+
+import com.org.test.service.SpeakingClockService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/speakingclock")
+public class SpeakingClockController {
+
+    @Autowired
+    private SpeakingClockService speakingClockService;
+
+    @GetMapping("/currentTimeWords")
+    public String getCurentTimeInWords(){
+        return speakingClockService.getCurrentTimeInWords("");
+    }
+
+    @GetMapping("/words/time/{time}")
+    public String getTimeInWords(@PathVariable("time") String time){
+        return speakingClockService.getCurrentTimeInWords(time);
+    }
+
+}
